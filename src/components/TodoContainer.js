74de435706d4583b33f9f225b51aaead35866124
Collins Tatang <makungongcollins@gmail.com>
2021-08-18 13:44:02 +0100

    fix linters

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,69 +2,72 @@ import React from 'react';
 import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
+
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
         id: 1,
-        title: "Setup development environment",
-        completed: true
+        title: 'Setup development environment',
+        completed: true,
       },
       {
         id: 2,
-        title: "Develop website and add content",
-        completed: false
+        title: 'Develop website and add content',
+        completed: false,
       },
       {
         id: 3,
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
-   };
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+
    handleChange = (id) => {
-     this.setState(prevState => ({
-       todos: prevState.todos.map(todo => {
-         if(todo.id === id){
-          return{
+     this.setState((prevState) => ({
+       todos: prevState.todos.map((todo) => {
+         if (todo.id === id) {
+           return {
              ...todo,
              completed: !todo.completed,
-          }
+           };
          }
-         return todo
+         return todo;
        }),
      }));
    };
-   delTodo = id => {
+
+   delTodo = (id) => {
+     this.setState({
+       todos: [
+         ...this.state.todos.filter((todo) => todo.id !== id),
+       ],
+     });
+   };
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false,
+    };
     this.setState({
-      todos: [
-        ...this.state.todos.filter(todo => {
-          return todo.id !== id;
-        })
-      ]
+      todos: [...this.state.todos, newTodo],
     });
   };
 
-  addTodoItem = title => {
-    const newTodo = {
-      id: 4,
-      title: title,
-      completed: false
-    };
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    });
-  };
-   render() {
+  render() {
     return (
       <div>
         <Header />
         <InputTodo />
-        <TodosList 
-        todos={this.state.todos} 
-        handleChangeProps={this.handleChange}
-        deleteTodoProps={this.delTodo}
-        addTodoProps={this.addTodoItem} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+          addTodoProps={this.addTodoItem}
+        />
       </div>
     );
   }
